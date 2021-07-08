eaa3c89fd924cc8078269b747733f93908566eef
Srinidhi Giri <86220566+SrinidhiGiri@users.noreply.github.com>
2021-07-08 09:56:18 +0530

    Add files via upload

---
+++ StudentForm/AdmissionForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";    
+    
+class AdmissionForm extends Component {    
+    render() {    
+        return (    
+            <h1>This Admission Form Component</h1>    
+        )    
+    }    
+}    
+    
+export default AdmissionForm;
